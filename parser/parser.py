e07b92e9c8b3ff7679baf31a260f7125711e8f08
sergei4e <4e.sergei@gmail.com>
2015-10-10 23:14:41 +0300

    Финальная версия html парсера с документацией и тестом

--- parser/parser.py
+++ parser/parser.py
@@ -1,30 +1,22 @@
 # coding: utf-8
 from lxml import html
-from regulars import regulars
-
-HTML = '''
-<html>
-<body>
-<div title="buyer-name">Carson Busses</div>
-<span class="item-price">$29.95</span>
-<span class="item-price">$14.95</span>
-<span id="item-price">
-    <span class="item-company">ООО Рога и копыта</span>
-</span>
-</body>
-</html>
-'''
-
-page1 = HTML
 
 
 def get_html_elements(page, regulars):
+    """
+    Функция парсинга html страницы по выражениям XPath
+    :param page: Страница которую нужно парсить
+    :param regulars: Словарь регулярных выражений XPath из файла 'regulars.py'
+    :return: Словарь вида {название параметра: спаршенное значение}
+
+    >>> HTML = '''<div title="buyer-name">Carson Busses</div>'''
+    >>> regulars = {'byers': '//div[@title="buyer-name"]/text()'}
+    >>> print get_html_elements(HTML, regulars)
+    {'byers': ['Carson Busses']}
+    """
+
     tree = html.fromstring(str(page))
     elements = {}
     for param in regulars.keys():
         elements[param] = tree.xpath(regulars[param])
     return elements
-
-p = get_html_elements(page1, regulars)
-
-print p
